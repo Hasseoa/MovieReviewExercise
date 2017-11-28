0a89abf46c9c80da941ac92190e3f4b8fa69ba6c
Aram <aramnr@yahoo.com>
2017-11-28 15:02:08 +0100

    något ändrat

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 let movieData = {
     imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRam_UTtsl8bwBxxKNeBtG-i5wyW2H0ozBndpMlcO8LSIYNTQ3J7w",
-    title: "The Hobbit (1977)",
+    title: 'The Hobbit (1977)',
     text: "Lorem ipsum dolor sit amet, dignissim mauris, facilisis et iverra volutpat nulla, vel lectus augue non accumsan metus libero."+
     "hfjkdhszjkfs hsdjhfsdjkh hsjfhsdjk sdfhfjk fhsjkfh jsdfjkf jsdf nsjkdfhjk fnsdjf sdfjn sdfj ghgsdfh ghsdfjhf gsdfhgdf hsdhkf sdfh,",
     actors:["John Huston","Orson Bean","Brother Theodor"], 
