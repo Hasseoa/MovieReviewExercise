9b578cd46f031c180c67a7630832305d9cb4ebec
Hans Bockgaard <hasseoa@gmail.com>
2017-11-28 15:58:39 +0100

    new discription text

--- data.js
+++ data.js
@@ -1,8 +1,7 @@
 let movieData = {
     imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRam_UTtsl8bwBxxKNeBtG-i5wyW2H0ozBndpMlcO8LSIYNTQ3J7w",
     title: "The Hobbit (1977)",
-    text: "Lorem ipsum dolor sit amet, dignissim mauris, facilisis et iverra volutpat nulla, vel lectus augue non accumsan metus libero."+
-    "hfjkdhszjkfs hsdjhfsdjkh hsjfhsdjk sdfhfjk fhsjkfh jsdfjkf jsdf nsjkdfhjk fnsdjf sdfjn sdfj ghgsdfh ghsdfjhf gsdfhgdf hsdhkf sdfh,",
+    text: "Bilbo Baggins the Hobbit was just minding his own business, when his occasional visitor Gandalf the Wizard drops in one evening . One by one, a whole group of dwarves drop in, and before he knows it, Bilbo has joined their quest to reclaim their kingdom, taken from them by an evil dragon named Smaug. The only problem is that Gandalf has told the dwarves that Bilbo is an expert burglar, but he isn't....",
     actors:["John Huston","Orson Bean","Brother Theodor"], 
 };
 
