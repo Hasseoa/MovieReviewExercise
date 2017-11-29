3935a1d9c578985e75439e46f0eb22ff7f2a6c6b
Hans Bockgaard <hasseoa@gmail.com>
2017-11-29 15:28:15 +0100

    add angular killed Jquery

--- code.ts
+++ code.ts
@@ -1,54 +1,26 @@
-// angular stuff
-
+// imports
 import * as angular from "angular";
+import {movieData} from "./data";
+import {Review} from "./interfaces";
+
+// angular stuff
 
 let app = angular.module("reviewApp", []);
 
 app.controller("reviewController", ($scope) => {
-  $scope.message = "Hello from Angular!";
-  $scope.greeting = "Hello world!";
+  $scope.movie = movieData;
+  $scope.rating = 0;
+  $scope.filled = [false, false, false, false, false];
+  $scope.changeStarRating = (newRating) => {
+    for (let i = 1; i <= $scope.rating; i++) {
+        $scope.filled[i] = false;
+    }
+    $scope.rating = newRating;
+    for (let i = 1; i <= newRating; i++) {
+        $scope.filled[i] = true;
+    }
+  };
+  $scope.handleClick = (e) => {
+      $scope.changeStarRating(e);
+    };
 });
-
-// imports
-
-import * as $ from "jquery";
-import {movieData} from "./data";
-import {Review} from "./interfaces";
-
-// handle data and stuff
-
-renderMovie(movieData);
-
-function renderMovie(movie: Review) {
-    $("#poster").attr("src", movie.imageSrc);
-    $("#title").text(movie.title);
-    $("#text").text(movie.text);
-
-    let actorList = "";
-    for (let i of movie.actors) {
-        const actor = movie.actors[i];
-        actorList += ("<li>");
-        actorList += actor;
-        actorList += ("</li>");
-    }
-    $("#actorList").html(actorList);
-}
-
-// Stars and stuff.....
-
-function changeStarRating(grade) {
-    for (let i = 1; i <= 5; i++ ) {
-        $("#star" + i).removeClass("filledStar");
-        $("#star" + i).addClass("emptyStar");
-    }
-    for (let i = 1; i <= grade; i++) {
-        $("#star" + i).removeClass("emptyStar");
-        $("#star" + i).addClass("filledStar");
-    }
-}
-
-for (let i = 1; i <= 5; i++) {
-      $("#star" + i).on("click", () => {
-          changeStarRating(i);
-      });
-  }
